c296755af9bb1a1c12744b493b6d76c167a45de7
aphil311 <41242437+aphil311@users.noreply.github.com>
2019-06-05 13:07:31 -0500

    Changed from interface to abstract class to make code more reuseable.

--- Player.java
+++ Player.java
@@ -1,4 +1,12 @@
-interface Player {
-  void move(int a);
-  int getVal();
+abstract class Player {
+  protected String icon;
+  protected int val;
+  Board board;
+  public Player(String symbol, int value, Board b) {
+    val = value;
+    icon = symbol;
+    board = b;
+  }
+  abstract void move();
+  abstract int getVal();
 }
