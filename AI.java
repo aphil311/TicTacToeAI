e0fb898bb61e7ea6a9f74c7b82a0913fe627fd1f
aphil311 <41242437+aphil311@users.noreply.github.com>
2019-06-11 10:45:32 -0500

    Revamped AI algorithm

--- AI.java
+++ AI.java
@@ -1,57 +1,148 @@
+import java.util.Random;
+
 public class AI extends Player{
-  public AI(String s, int v, Board b) {
-    super(s, v, b);
+  public AI(String s, int v, Board b, int c) {
+    super(s, v, b, c);
   }
   public void move() {
-    int x=0;
-    int y=0;
-    if(findWin(val)!=null) {
-      x=findWin(val)[0];
-      y=findWin(val)[1];
+    if(winExists()[0]>=0) {
+      System.out.println("Found a winning move!");
+      board.setPos(winExists()[0], winExists()[1], val, icon);
     }
-    board.setPos(x,y, val, icon);
+    else {
+      defaultMove();
+    }
   }
-  private int[] findWin(int a) {
+
+  private int[] winExists() {
+    int[] points = {-1, -1};
     for(int i=0; i<3; i++) {
-      if(board.addRow(i)==a*2) {
-        int[] points = {i, findOpenCol(i)};
-        return points;
+      if(board.addRow(i)==val*2) {
+        points[0]=i;
+        for(int j = 0; j<3; j++) {
+          if(board.getPos(i,j)==0) {
+            points[1]=j;
+            return points;
+          }
+        }
       }
-      if(board.addCol(i)==2*val) {
-        int[] points = {findOpenRow(i), i};
-        return points;
+      if(board.addCol(i)==val*2) {
+        points[1]=i;
+        for(int j = 0; j<3; j++) {
+          if(board.getPos(j,i)==0) {
+            points[0]=j;
+            return points;
+          }
+        }
+      }
+      if(board.addRow(i)==opp*2) {
+        points[0]=i;
+        for(int j = 0; j<3; j++) {
+          if(board.getPos(i,j)==0) {
+            points[1]=j;
+            return points;
+          }
+        }
+      }
+      if(board.addCol(i)==opp*2) {
+        points[1]=i;
+        for(int j = 0; j<3; j++) {
+          if(board.getPos(j,i)==0) {
+            points[0]=j;
+            return points;
+          }
+        }
       }
     }
-    if(board.addDiag(0)==2*val) {
+    if(board.addDiag(0)==6||board.addDiag(0)==20)
+      playDiag(0);
+    if(board.addDiag(1)==6||board.addDiag(1)==20)
+      playDiag(1);
+
+    return points;
+  }
+
+  private void defaultMove() {
+    if(board.getPos(1, 1)==0) {
+      System.out.println("Playing center!");
+      board.setPos(1, 1, val, icon);
+    }
+    else {
+      attemptCorner();
+    }
+  }
+
+  private void attemptCorner() {
+    int i=0;
+    int j=2;
+    if (board.getPos(i, j)==0) {
+      System.out.println("Option 1");
+      System.out.println(board);
+      board.setPos(i, j, val, icon);
+      System.out.println(board);
+      return;
+    }
+    else if (board.getPos(i, i)==0) {
+      System.out.println("Option 2");
+      System.out.println(board);
+      board.setPos(i, i, val, icon);
+      System.out.println(board);
+      return;
+    }
+    else if (board.getPos(j, j)==0) {
+      System.out.println("Option 3");
+      System.out.println(board);
+      board.setPos(j, j, val, icon);
+      System.out.println(board);
+      return;
+    }
+    else if (board.getPos(j, i)==0) {
+      System.out.println("Option 4");
+      board.setPos(j, i, val, icon);
+      return;
+    }
+    
+  }
+
+  private void playSide() {
+    if (board.getPos(0, 1)==0) {
+      board.setPos(0, 1, val, icon);
+      return;
+    }
+    else if (board.getPos(1, 2)==0) {
+      board.setPos(1, 2, val, icon);
+      return;
+    }
+    else if (board.getPos(2, 1)==0) {
+      board.setPos(2, 1, val, icon);
+      return;
+    }
+    else if (board.getPos(1, 0)==0) {
+      board.setPos(1, 0, val, icon);
+      return;
+    }
+  }
+
+  private void playDiag(int a) {
+    if(a==0) {
       for(int i=0; i<3; i++) {
-        if(board.getPos(i,i)==0) {
-          int[] points = {i, i};
-          return points;
+        if (board.getPos(i, i)==0){
+          board.setPos(i, i, val, icon);
+          return;
         }
       }
     }
-    if(board.addDiag(1)==2*val) {
-      for(int i=2; i>=0; i--) {
-        if(board.getPos(i,2-i)==0){
-          int[] points = {i, 2-i};
-          return points;
+    else if(a==1) {
+      for(int i=0; i<3; i++) {
+        for (int j=2; j>=0; j--) {
+          if(board.getPos(i, j)==0)
+            board.setPos(i, j, val, icon);
         }
       }
     }
-    return null;
-  }
-  private int findOpenCol(int r) {
-    for(int i = 0; i<3; i++) {
-      if(board.getPos(r,i)==0)
-        return i;
+    else {
+      System.out.println("Error");
+      defaultMove();
     }
-    return -1;
-  }
-  private int findOpenRow(int c) {
-    for(int i = 0; i<3; i++) {
-      if(board.getPos(i,c)==0)
-        return i;
-    }
-    return -1;
   }
 }
